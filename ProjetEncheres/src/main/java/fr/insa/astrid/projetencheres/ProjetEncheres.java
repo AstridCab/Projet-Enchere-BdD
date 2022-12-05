decd3d1ad14dc4964dd733ed9fc55911f3ab6e9d
Utilisateur <Utilisateur@172.17.6.201>
2022-12-05 09:42:55 +0100

    nouv annonce vaadin

--- ProjetEncheres/src/main/java/fr/insa/astrid/projetencheres/ProjetEncheres.java
+++ ProjetEncheres/src/main/java/fr/insa/astrid/projetencheres/ProjetEncheres.java
@@ -78,7 +78,7 @@ public class ProjetEncheres {
                     create table annonce (
                         id integer not null primary key
                         generated always as identity,
-                        nom varchar(50) not null,
+                        titre varchar(50) not null,
                         description varchar(80) not null,
                         prixInitial integer not null,
                         dateDebutEnchere timestamp,
@@ -208,7 +208,7 @@ public class ProjetEncheres {
         suppSchema(con);
         creeSchema(con);
     }
-    public static void nouvUtilisateur(Connection con, String pseudo, String nomUtilisateur, String prenom, int codePostal, String email, String pass) throws SQLException { // Permet de demander des informations à l'utilisateur qui seront rentrées dans la table
+    public static void nouvUtilisateur(Connection con, String pseudo, String nom, String prenom, int codePostal, String email, String pass) throws SQLException { // Permet de demander des informations à l'utilisateur qui seront rentrées dans la table
         con.setAutoCommit(false);
         try ( PreparedStatement pst = con.prepareStatement("insert into utilisateur (pseudo,nom,prenom,codePostal,email,pass) values (?,?,?,?,?,?)")) {
             System.out.println("Pseudo d'utilisateur :");
@@ -224,7 +224,7 @@ public class ProjetEncheres {
             System.out.println("Mot de pass :");
             //String pass = Lire.S();
             pst.setString(1, pseudo);
-            pst.setString(2, nomUtilisateur);
+            pst.setString(2, nom);
             pst.setString(3, prenom);
             pst.setInt(4, codePostal);
             pst.setString(5, email);
@@ -238,36 +238,34 @@ public class ProjetEncheres {
             con.setAutoCommit(true);
         }
     }
-    public static void nouvAnnonce(Connection con) throws SQLException { // Permet de demander des informations à l'utilisateur qui seront rentrées dans la table Annonce
+    public static void nouvAnnonce(Connection con, String pseudo, String titre, String description, Timestamp dateDebutEnchere, Timestamp dateFinEnchere, String type, int prixInitial) throws SQLException { // Permet de demander des informations à l'utilisateur qui seront rentrées dans la table Annonce
         con.setAutoCommit(false);
-        try ( PreparedStatement pst = con.prepareStatement("insert into annonce (nom,description,prixInitial,dateDebutEnchere,dateFinEnchere,idUtilisateur,idType) values (?,?,?,?,?,?,?)")) {
+        try ( PreparedStatement pst = con.prepareStatement("insert into annonce (idUtilisateur,titre,description,dateDebutEnchere,dateFinEnchere,idType,prixInitial) values (?,?,?,?,?,?,?)")) {
+            System.out.println("Pseudo d'Utilisateur :");
+            //String pseudo = Lire.S();
             System.out.println("Nom de l'objet :");
-            String nom = Lire.S();
+            //String titre = Lire.S();
             System.out.println("Description de l'objet :");
-            String description = Lire.S();
-            System.out.println("Prix de mise en vente");
-            int prixInitial = Lire.i();
+            //String description = Lire.S();
             System.out.println("Date de mise en vente (sous la forme AAAA-MM-JJ hh:mm:ss) ");
-            String dateDebutEnchere = Lire.S();
-            Timestamp dateDebut = Timestamp.valueOf(dateDebutEnchere); // permet de convertir le string en timestamp
+            //String dateDebutEnchere = Lire.S();
+            //Timestamp dateDebut = Timestamp.valueOf(dateDebutEnchere); // permet de convertir le string en timestamp
             System.out.println("Date de fin de mise en vente (sous la forme AAAA-MM-JJ hh:mm:ss) ");
-            String dateFinEnchere = Lire.S();
-            Timestamp dateFin = Timestamp.valueOf(dateFinEnchere);
-            System.out.println("Nom d'Utilisateur :");
-            String nomUtilisateur = Lire.S();
-            System.out.println("Mot de pass :");
-            String pass = Lire.S();
+            //String dateFinEnchere = Lire.S();
+            //Timestamp dateFin = Timestamp.valueOf(dateFinEnchere);
             System.out.println("Catégorie de votre objet entre vetements, meuble, bijoux, art :");
-            String type = Lire.S();
-            int idUtilisateur = identifiantUtilisateur(con, nomUtilisateur,pass);
+            //String type = Lire.S();
+            System.out.println("Prix de mise en vente");
+            //int prixInitial = Lire.i();
+            int idUtilisateur = identifiantUtilisateur(con,pseudo);
             int idCategorie= identifiantCategorie(con, type);
-            pst.setString(1, nom);
-            pst.setString(2, description);
-            pst.setInt(3, prixInitial);
-            pst.setTimestamp(4, dateDebut);
-            pst.setTimestamp(5, dateFin);
-            pst.setInt(6, idUtilisateur);
-            pst.setInt(7, idCategorie);
+            pst.setInt(1, idUtilisateur);
+            pst.setString(2, titre);
+            pst.setString(3, description);
+            pst.setTimestamp(4, dateDebutEnchere);
+            pst.setTimestamp(5, dateFinEnchere);
+            pst.setInt(6, idCategorie);
+            pst.setInt(7, prixInitial);
             pst.executeUpdate();
         } catch(SQLException ex){
             con.rollback();
@@ -277,12 +275,11 @@ public class ProjetEncheres {
             con.setAutoCommit(true);
         }
     }
-    public static int identifiantUtilisateur(Connection con, String nomUtilisateur, String pass)throws SQLException {
+    public static int identifiantUtilisateur(Connection con, String pseudo)throws SQLException {
         con.setAutoCommit(false);
         int id = -1 ; //cela permet au programme de renvoyer une valeur même s'il y a une erreur avec le rollback (-1 car cette valeur est impossible dans le tableau id)
-        try ( PreparedStatement pst = con.prepareStatement("select id from utilisateur where (nom,pass)=(?,?)")) {
-            pst.setString(1, nomUtilisateur);
-            pst.setString(2, pass);
+        try ( PreparedStatement pst = con.prepareStatement("select id from utilisateur where (pseudo)=(?)")) {
+            pst.setString(1, pseudo);
             try (ResultSet tableUtilisateur= pst.executeQuery()){ //on écrit rien dans les parenthèses car le programme sait deja ce qu'il doit envoyer                
                         while (tableUtilisateur.next()){ //tant qu'il reste des lignes le programme continue
                             id = tableUtilisateur.getInt("id");
