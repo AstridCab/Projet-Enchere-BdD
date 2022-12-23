cff65f43be8851c511f851e8069054926d4ad535
ugo-bonnet <80956091+ugo-bonnet@users.noreply.github.com>
2022-12-23 14:41:25 +0100

    affichage des utilisateurs dans une grid

--- ProjetEncheres/src/main/java/fr/insa/astrid/vaadin/VueUtilisateurs.java
+++ ProjetEncheres/src/main/java/fr/insa/astrid/vaadin/VueUtilisateurs.java
@@ -5,12 +5,18 @@
 package fr.insa.astrid.vaadin;
 
 
+import Objets.Utilisateur;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import fr.insa.astrid.projetencheres.ProjetEncheres;
+import static fr.insa.astrid.projetencheres.ProjetEncheres.defautConnect;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,16 +28,17 @@ import java.util.List;
 // VUE AVEC TOUTES LES ANNONCES
 
 @Route(value = "vueUtilisateurs", layout = MainLayout.class)
-@PageTitle("utilisateurs")
+@PageTitle("Utilisateurs")
 
 public class VueUtilisateurs extends VerticalLayout{
     
-    Grid <Personne> grid = new Grid<>(Personne.class);
+    Grid <Utilisateur> grid = new Grid<>(Utilisateur.class);
     
     TextField filtreRecherche_tf = new TextField();
     
-    List <Personne> listePersonnes;
-   
+    List <Utilisateur> listePersonnes = new ArrayList<Utilisateur>();
+    
+    Utilisateur utilisateur = new Utilisateur();
     
     public VueUtilisateurs(){
         
@@ -39,46 +46,58 @@ public class VueUtilisateurs extends VerticalLayout{
         
         configureGrid();
         configureFiltreRecherche();
-        
-        updateGrid();
-        
-        this.add(grid);
-                    
+//        
+//        updateGrid();
+//        
+//        UtilisateurForm form = new UtilisateurForm();
+//        
+//        HorizontalLayout hLayout = new HorizontalLayout(grid,form);
+//        hLayout.setSizeFull();
+//        
+//        VerticalLayout vLayout = new VerticalLayout(filtreRecherche_tf,hLayout);
+//        vLayout.setSizeFull();
+//        
+//        this.add(vLayout);
+
+          //grid.addColumn(Utilisateur::getPseudo);
+          afficheUtilisateurs(utilisateur);
+          add(grid);
+          
     }
 
     private void configureGrid() {
         
         grid.setSizeFull();
-        grid.setColumns("idUtilisateur", "nom", "prenom", 
-                "adresseMail", "codePostal");
+        grid.setColumns("pseudo", "nom", "prenom", 
+                "email", "codePostal");
         grid.setAllRowsVisible(true);
-        
-        
-    }
-    
-    public Personne ajoutPersonne(){
-        Personne p1 = new Personne() {};
-        
-        p1.setIdUtilisateur(1);
-        p1.setIdAnnonceOwned(2);
-        p1.setPrenom("Jean");
-        p1.setNom("Michel");
-        p1.setAdresseMail("jeanmichel@gmail.com");
-        p1.setCodePostal(67000);
-        p1.setPassword("pass");
-        
-        return p1;
+              
     }
 
-    public void updateGrid() {
-        Personne p1 = ajoutPersonne();
-        listePersonnes.add(p1);
-        grid.setItems(listePersonnes);
-    }
+//    public void updateGrid() {
+//        Utilisateur p1 = ajoutPersonne();
+//        //listePersonnes.add(p1);
+//        //grid.setItems(listePersonnes);
+//    }
 
     private void configureFiltreRecherche() {
         filtreRecherche_tf.setPlaceholder("Filtre par nom...");
         filtreRecherche_tf.setClearButtonVisible(true);
         filtreRecherche_tf.setValueChangeMode(ValueChangeMode.LAZY);
     }
+
+    private void afficheUtilisateurs(Utilisateur utilisateur) {
+                    
+        List<Utilisateur> listeUtilisateurs = new ArrayList<Utilisateur>();
+        
+        try (Connection con = defautConnect()) {
+            listeUtilisateurs = ProjetEncheres.afficherUtilisateur(con, utilisateur);       
+        } catch (Exception ex) {
+            throw new Error(ex);
+        }
+        int L = listeUtilisateurs.size();
+        
+        grid.setItems(listeUtilisateurs);
+        }
+    
 }
