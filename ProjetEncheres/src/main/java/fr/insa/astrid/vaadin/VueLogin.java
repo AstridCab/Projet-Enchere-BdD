b0e976f13210d53c173e518e7d7f3358f2e6a314
ugo-bonnet <80956091+ugo-bonnet@users.noreply.github.com>
2022-12-24 18:54:48 +0100

    quand on se connecte si le mdp ou le id n'est pas le bon ça se connecte pas

--- ProjetEncheres/src/main/java/fr/insa/astrid/vaadin/VueLogin.java
+++ ProjetEncheres/src/main/java/fr/insa/astrid/vaadin/VueLogin.java
@@ -1,12 +1,17 @@
 package fr.insa.astrid.vaadin;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -17,23 +22,55 @@ import com.vaadin.flow.router.Route;
 @PageTitle("Connection")
 public class VueLogin extends VerticalLayout {
 
-	private final LoginForm connection = new LoginForm();
-
-	public VueLogin(){
+    private final LoginForm connection = new LoginForm();
+    LoginOverlay loginOverlay = new LoginOverlay();
+    
+    public VueLogin(){
                 
-                // pour être beau et au centre
-		setSizeFull();
-		setAlignItems(FlexComponent.Alignment.CENTER); 
-		setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-
-		// ça envoie sur la page du menu quand on se connecte
-                connection.setAction("Menu");  
-
-		VerticalLayout vLayout = new VerticalLayout();
-		vLayout.add(new H1("Connection"), new Span("Nom d'utilisateur : user"), new Span("Mot de passe: pass"));
-		vLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-
-		add(vLayout, connection);
+        // pour être beau et au centre
+	setSizeFull();
+	setAlignItems(FlexComponent.Alignment.CENTER); 
+	setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        // ça envoie sur la page du menu quand on se connecte
+        //connection.setAction("Menu");  
+	//VerticalLayout vLayout = new VerticalLayout();
+	//vLayout.add(new H1("Connection"), new Span("Nom d'utilisateur : user"), new Span("Mot de passe: pass"));
+        //vLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        
+        loginOverlay.setTitle("ibuy");
+        loginOverlay.setDescription("Notre site de vente aux enchères");
+        loginOverlay.setOpened(true);
+        
+        add(connection,loginOverlay);
+        
+        testConnection();
+        
 	}
+
+    private void testConnection() {
+        
+        loginOverlay.addLoginListener(event -> {
+           String username = event.getUsername();
+           String password = event.getPassword();
+           
+           String username_BDD = "user";
+           String password_BDD = "pass";
+           
+           if ((password.equals(password_BDD)) & (username.equals(username_BDD))) {            
+               UI.getCurrent().close();
+               UI.getCurrent().navigate("Menu");
+
+               //Notification.show("c'est ok !");
+               
+               
+           } else {
+               Notification.show("Mot de passe ou identifiant incorrect");
+               loginOverlay.setError(true);
+               
+            }
+           
+        });
+        
+    }
 
 }
